5f715f26845d59a2a79e1505f0a4b51de3c94f44
boiwindihotspot <boiwindihotspot@gmail.com>
2025-12-03 12:32:28 +0100

    sidebar

--- src/layouts/full/sidebar/sidebarItem.ts
+++ src/layouts/full/sidebar/sidebarItem.ts
@@ -3,5 +3,15 @@ export default [
     title: 'Dashboard',
     icon: 'fa-solid fa-chart-area',
     path: '/'
+  },
+  {
+    title: 'Tenants',
+    icon: 'fa-solid fa-chart-area',
+    path: '/'
+  },
+  {
+    title: '  Settings',
+    icon: 'fa-solid fa-chart-area',
+    path: '/'
   }
 ]
